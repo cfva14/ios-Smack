3c4d644bc58a0d6e66eb270c57d85431cdfb57bd
Carlos Valencia <Carlos@Carloss-MacBook-Air.local>
2017-08-06 20:41:50 -0700

    Made register call.

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func pickAvatarButtonPressed(_ sender: Any) {
+    
+    }
+    @IBAction func pickBGColorButtonPressed(_ sender: Any) {
+    
+    }
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        guard let email = emailText.text , emailText.text != "" else { return }
+        guard let password = passwordText.text , passwordText.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
     @IBAction func closeButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Carlos Valencia on 8/6/17.
+//  Copyright © 2017 Carlos Valencia. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
